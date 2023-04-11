ce740885c75f1977af022310aeafb8040cff2e8e
Florian Schmid <melonencool@gmail.com>
2023-04-11 13:00:03 +0200

    get random cpu

--- game.js
+++ game.js
@@ -1,15 +1,28 @@
+var cpuList;
+var currentCpu;
+var nextCpu;
+
 async function main() {
-    var cpuList;
     await fetch('./data.json')
         .then((response) => response.json())
         .then((json) => cpuList = json);
 
-    
+    currentCpu = getRandomCpu();
+    nextCpu = getRandomCpu();
 }
 
 function getRandomCpu() {
-
+    let randomIndex = getRandomInt(0, cpuList.length)
+    return {
+        name: cpuList[randomIndex]["name"].split('@')[0],
+        score: cpuList[randomIndex]["cpuScore"]
+    }
 }
 
+function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min) + min); // The maximum is exclusive and the minimum is inclusive
+}
 
 main();
